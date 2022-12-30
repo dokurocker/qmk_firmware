b30cc9bce9c62afaab9251aba74d8d7040f47482
dokurocker <dokurocker@gmail.com>
2022-12-31 03:33:55 +0900

    な行チェック調整

--- keyboards/crkbd/keymaps/dokurocker/dvorakjp/dvorakjp.c
+++ keyboards/crkbd/keymaps/dokurocker/dvorakjp/dvorakjp.c
@@ -1,44 +1,42 @@
 #include "dvorakjp.h"
-//wh k *y kw qw gw xca xka xke sw zw ts tw dw xtu hw xwa
+
 static roma roma_list[] = {
     {DV_Y, X2HEX(X_Y), X2HEX(X_Y), 1, 2}, {DV_Y, X2HEX(X_Y), X2HEX(X_Y), 50, 50},
-    // 単一のN（ん）の後の子音（UNNCO -> UNCOと打てるように）
-    {DV_N, X2HEX(X_N), X2HEX(X_N), 1, 15},
-    // h を y にする
-    {DV_H, X2HEX(X_H), X2HEX(X_Y), 52, 1},
-    {DV_A, X2HEX(X_A), X2HEX(X_A), 0, 1}, {DV_I, X2HEX(X_I), X2HEX(X_I), 0, 1}, {DV_U, X2HEX(X_U), X2HEX(X_U), 0, 1}, {DV_E, X2HEX(X_E), X2HEX(X_E), 0, 1}, {DV_O, X2HEX(X_O), X2HEX(X_O), 0, 1},
-    // 二重母音拡張と撥音拡張
-    {DV_QUOT, X2HEX(X_I), X2HEX(X_A), 0xff, 1},
-    {DV_COMM, X2HEX(X_U), X2HEX(X_O), 0xff, 1},
-    {DV_DOT, X2HEX(X_I), X2HEX(X_E), 0xff, 1},
-    {DV_SCLN, X2HEX(X_N), X2HEX(X_A), 0xff, 1},
-    {DV_Q, X2HEX(X_N), X2HEX(X_O), 0xff, 1}, 
-    {DV_J, X2HEX(X_N), X2HEX(X_E), 0xff, 1},
-    {DV_K, X2HEX(X_N), X2HEX(X_U), 0xff, 1},
-    {DV_X, X2HEX(X_N), X2HEX(X_I), 0xff, 1},
+    // 単一のN（ん）の後の子音をチェックできるように0xfeで特別な対応（UNNCO -> UNCOと打てるように）
+    {DV_N, X2HEX(X_N), X2HEX(X_N), 0xfe, 1},
+    // 子音
     // n を y にする
-    {DV_F, X2HEX(X_F), X2HEX(X_F), 1, 2}, {DV_F, X2HEX(X_F), X2HEX(X_F), 36, 36},
-    {DV_G, X2HEX(X_G), X2HEX(X_G), 1, 2}, {DV_G, X2HEX(X_G), X2HEX(X_G), 34, 34},
-    {DV_C, X2HEX(X_C), X2HEX(X_K), 1, 2}, {DV_C, X2HEX(X_C), X2HEX(X_K), 32, 32},
-    {DV_D, X2HEX(X_D), X2HEX(X_D), 1, 2}, {DV_D, X2HEX(X_D), X2HEX(X_D), 3, 30},
-    {DV_T, X2HEX(X_T), X2HEX(X_T), 1, 2}, {DV_T, X2HEX(X_T), X2HEX(X_T), 1, 28},
-    {DV_H, X2HEX(X_H), X2HEX(X_H), 1, 28},
-    {DV_H, X2HEX(X_H), X2HEX(X_H), 1, 2}, {DV_H, X2HEX(X_H), X2HEX(X_H), 25, 25},
-    {DV_B, X2HEX(X_B), X2HEX(X_B), 1, 2}, {DV_B, X2HEX(X_B), X2HEX(X_B), 23, 23},
-    {DV_M, X2HEX(X_M), X2HEX(X_M), 1, 2}, {DV_M, X2HEX(X_M), X2HEX(X_M), 21, 21},
+    {DV_F, X2HEX(X_F), X2HEX(X_F), 1, 2}, {DV_F, X2HEX(X_F), X2HEX(X_F), 47-4, 47-4},
+    {DV_G, X2HEX(X_G), X2HEX(X_G), 1, 2}, {DV_G, X2HEX(X_G), X2HEX(X_G), 45-5, 45-5},
+    {DV_C, X2HEX(X_C), X2HEX(X_K), 1, 2}, {DV_C, X2HEX(X_C), X2HEX(X_K), 43-5, 43-5},
+    {DV_D, X2HEX(X_D), X2HEX(X_D), 1, 2}, {DV_D, X2HEX(X_D), X2HEX(X_D), 41-6, 41-6},
+    {DV_T, X2HEX(X_T), X2HEX(X_T), 1, 2}, {DV_T, X2HEX(X_T), X2HEX(X_T), 39-10, 39-10},
+    {DV_H, X2HEX(X_H), X2HEX(X_H), 1, 2}, {DV_H, X2HEX(X_H), X2HEX(X_H), 37-5, 37-5},
+    {DV_B, X2HEX(X_B), X2HEX(X_B), 1, 2}, {DV_B, X2HEX(X_B), X2HEX(X_B), 35-4, 35-4},
+    {DV_M, X2HEX(X_M), X2HEX(X_M), 1, 2}, {DV_M, X2HEX(X_M), X2HEX(X_M), 33-4, 33-4},
     // h を y にする
-    {DV_P, X2HEX(X_P), X2HEX(X_P), 1, 2}, {DV_P, X2HEX(X_P), X2HEX(X_P), 20, 20},
-    {DV_R, X2HEX(X_R), X2HEX(X_R), 1, 2}, {DV_R, X2HEX(X_R), X2HEX(X_R), 18, 18},
-    {DV_L, X2HEX(X_L), X2HEX(X_L), 1, 2}, {DV_L, X2HEX(X_L), X2HEX(X_L), 16, 16},
-    {DV_S, X2HEX(X_S), X2HEX(X_S), 1, 2}, {DV_S, X2HEX(X_S), X2HEX(X_S), 14, 14},
-    {DV_Q, X2HEX(X_Q), X2HEX(X_Q), 1, 2}, {DV_Q, X2HEX(X_Q), X2HEX(X_Q), 12, 12},
-    {DV_J, X2HEX(X_J), X2HEX(X_J), 1, 2}, {DV_J, X2HEX(X_J), X2HEX(X_J), 10, 10},
-    {DV_X, X2HEX(X_X), X2HEX(X_X), 1, 2}, {DV_X, X2HEX(X_X), X2HEX(X_X), 8, 8},
-    {DV_W, X2HEX(X_W), X2HEX(X_W), 1, 2}, {DV_W, X2HEX(X_W), X2HEX(X_W), 6, 6},
-    {DV_V, X2HEX(X_V), X2HEX(X_V), 1, 2}, {DV_V, X2HEX(X_V), X2HEX(X_V), 4, 4},
-    {DV_Z, X2HEX(X_Z), X2HEX(X_Z), 1, 0}, {DV_Z, X2HEX(X_Z), X2HEX(X_Z), 2, 2},
-    {DV_N, X2HEX(X_N), X2HEX(X_Y), 2, 2},
-    {DV_H, X2HEX(X_H), X2HEX(X_Y), 1, 1},
+    {DV_P, X2HEX(X_P), X2HEX(X_P), 1, 2}, {DV_P, X2HEX(X_P), X2HEX(X_P), 31-2, 31-2},
+    {DV_R, X2HEX(X_R), X2HEX(X_R), 1, 2}, {DV_R, X2HEX(X_R), X2HEX(X_R), 29-2, 29-2},
+    {DV_L, X2HEX(X_L), X2HEX(X_P), 1, 2}, {DV_L, X2HEX(X_L), X2HEX(X_P), 27-2, 27-2},
+    {DV_S, X2HEX(X_S), X2HEX(X_S), 1, 2}, {DV_S, X2HEX(X_S), X2HEX(X_S), 25-3, 25-3},
+    {DV_Q, X2HEX(X_Q), X2HEX(X_Q), 1, 2}, {DV_Q, X2HEX(X_Q), X2HEX(X_Q), 23-3, 23-3},
+    {DV_J, X2HEX(X_J), X2HEX(X_J), 1, 2}, {DV_J, X2HEX(X_J), X2HEX(X_J), 21-2, 21-2},
+    {DV_K, X2HEX(X_K), X2HEX(X_K), 1, 2}, {DV_K, X2HEX(X_K), X2HEX(X_K), 19-3, 19-3},
+    {DV_X, X2HEX(X_X), X2HEX(X_X), 1, 2}, {DV_X, X2HEX(X_X), X2HEX(X_X), 17-9, 17-9},
+    {DV_W, X2HEX(X_W), X2HEX(X_W), 1, 2}, {DV_W, X2HEX(X_W), X2HEX(X_W), 15-6, 15-6},
+    {DV_V, X2HEX(X_V), X2HEX(X_V), 1, 2}, {DV_V, X2HEX(X_V), X2HEX(X_V), 13-2, 13-2},
+    {DV_Z, X2HEX(X_Z), X2HEX(X_Z), 1, 0}, {DV_Z, X2HEX(X_Z), X2HEX(X_Z), 11-3, 11-3},
+    // 拗音
+    {DV_S, X2HEX(X_S), X2HEX(X_S), 10, 4},  // ツァ行(ts)
+    // ヵ,ヶ,っ x[ck](xca xka xce xke) xt(xtu)
+    {DV_C, X2HEX(X_C), X2HEX(X_K), 9, 1}, {DV_K, X2HEX(X_K), X2HEX(X_K), 8, 1}, {DV_T, X2HEX(X_T), X2HEX(X_T), 7, 6},
+    {DV_H, X2HEX(X_H), X2HEX(X_H), 6, 1}, // テャ,デャ,ウァ行([tdw]h)
+    {DV_W, X2HEX(X_W), X2HEX(X_W), 5, 1}, // クァ,グァ,トァ,ドァ,ファ[cgtdh]w
+    {DV_N, X2HEX(X_N), X2HEX(X_Y), 4, 3},
+    {DV_W, X2HEX(X_W), X2HEX(X_W), 3, 1}, // クァ,スァ,ズァ,ヮ[kqszx]w(xwa)
+    {DV_H, X2HEX(X_H), X2HEX(X_Y), 2, 1},
+    {DV_Y, X2HEX(X_Y), X2HEX(X_Y), 1, 1},
+    // 母音
     {DV_A, X2HEX(X_A), X2HEX(X_A), 0, 1}, {DV_I, X2HEX(X_I), X2HEX(X_I), 0, 1}, {DV_U, X2HEX(X_U), X2HEX(X_U), 0, 1}, {DV_E, X2HEX(X_E), X2HEX(X_E), 0, 1}, {DV_O, X2HEX(X_O), X2HEX(X_O), 0, 1},
     // 二重母音拡張と撥音拡張
     {DV_QUOT, X2HEX(X_I), X2HEX(X_A), 0xff, 1},
@@ -50,7 +48,10 @@ static roma roma_list[] = {
     {DV_K, X2HEX(X_N), X2HEX(X_U), 0xff, 1},
     {DV_X, X2HEX(X_N), X2HEX(X_I), 0xff, 0}
 };
-
+// 単一nでな行チェック
+// n なら な行チェックし、な行じゃなければ、次は子音
+static roma* line_na = roma_list + 40;
+static roma* next = NULL;
 static roma* current = roma_list;
 static char raw_stack[6] = {'\0', '\0', '\0', '\0', '\0', '\0'};
 static unsigned char raw_count = 0;
@@ -91,6 +92,12 @@ bool dvorakjp(uint16_t keycode, uint8_t tapping_term)
                 djp_stack[djp_count++] = current->dvorakjp;
                 djp_stack[djp_count] = '\0';
             }
+            if (current->hit == 0xfe) {
+                // 次の入力で「な」行チェック
+                current = line_na;
+                next = current + current->miss;
+                return raw_count > 0;
+            }
             if (current->hit > 0 && current->hit != 0xff) {
                 current = current + current->hit;
                 return raw_count > 0;
@@ -112,6 +119,12 @@ bool dvorakjp(uint16_t keycode, uint8_t tapping_term)
             return false;
         }
         if (current->miss == 0) {
+            if (next != NULL) {
+                // 「な」行ではなかったので子音チェック
+                current = next;
+                next = NULL;
+                continue;
+            }
             break;
         }
         current = current + current->miss;
